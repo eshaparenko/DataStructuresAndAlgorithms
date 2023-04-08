5f020e034c0bf02978614c288704c93835573460
eshaparenko <gesha8610@gmail.com>
2023-04-08 21:23:27 +0300

    Implemented array

---
+++ app.js
@@ -0,0 +1,45 @@
+//Array implementation
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        let lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index)
+        return item;
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+}
+ 
+const arr = new MyArray()
+arr.push('hi')
+arr.push('you')
+arr.delete(1)
+console.log(arr.data);
